55921b530320af5409e1c114881429e00ad4d2a2
Rohit <rohitoffical0702@gmail.com>
2020-12-25 20:47:24 +0530

    add start scipt

--- public/Javascript/clusterMap.js
+++ public/Javascript/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [28.63535, 77.29098],
     zoom: 3
 });
 
