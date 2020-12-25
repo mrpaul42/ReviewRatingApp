bebd4804c86bbecdeeab6024837671c7f6c28c00
Rohit <rohitoffical0702@gmail.com>
2020-12-25 18:17:52 +0530

    ready to try deploying

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require('connect-mongo')(session);
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelpcamp'
-
+const dbUrl = 'mongodb://localhost:27017/yelpcamp'
+// process.env.DB_URL || 
 const expressError = require('./utility/expressError');
 const mongoose = require('mongoose');
 
